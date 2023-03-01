97838a04c773df4bb49a70061ccc4fe6be5fa420
Manasvi Pant <manasvipant1106@gmail.com>
2023-03-02 01:05:57 +0530

    Initial Commit

---
+++ Arrays/PeakElement.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int findPeakElement(int A[],int n){
+    cout<<"The peak element is: ";
+    if(n==1){
+        return A[0];
+    }
+    if(n==2){
+        if (A[0] >= A[1]){
+        return 0;
+        }
+        if (A[n - 1] >= A[n - 2]){
+            return A[n - 1];
+        }
+    }
+    for(int i=1;i<n-1;i++){
+        if(A[i]>=A[i-1] && A[i]>=A[i+1]){
+            return A[i];
+        }
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the number of elements you want in your array: ";
+    cin>>n;
+    cout<<"Enter the elements of the array: "<<endl;
+    int A[n];
+    for(int i=0;i<n;i++){
+        cin>>A[i];
+    }
+    cout<<findPeakElement(A,n);
+    return 0;
+}
